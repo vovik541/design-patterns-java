c132b3cd96401ac907777923dfdea7247c6ff665
vovik541 <conanzhill@gmail.com>
2025-12-19 04:44:36 -0800

    adapter example reviewed & added

---
+++ src/structural/adapter/adapters/SquarePegAdapter.java
@@ -0,0 +1,19 @@
+package structural.adapter.adapters;
+
+import structural.adapter.round.RoundPeg;
+import structural.adapter.square.SquarePeg;
+
+public class SquarePegAdapter extends RoundPeg {
+    private SquarePeg peg;
+
+    public SquarePegAdapter(SquarePeg peg) {
+        this.peg = peg;
+    }
+
+    @Override
+    public double getRadius() {
+        double result;
+        result = (Math.sqrt(Math.pow((peg.getWidth() / 2), 2) * 2));
+        return result;
+    }
+}
