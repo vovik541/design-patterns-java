f69bfa94f79f60563b8f766fec1e77d4a90803a7
vovik541 <conanzhill@gmail.com>
2025-12-19 19:24:12 -0800

    proxy example reviewed & added

---
+++ src/structural/proxy/someCoolMediaLibrary/ThirdPartyYouTubeLib.java
@@ -0,0 +1,9 @@
+package structural.proxy.someCoolMediaLibrary;
+
+import java.util.HashMap;
+
+public interface ThirdPartyYouTubeLib {
+    HashMap<String, Video> popularVideos();
+
+    Video getVideo(String videoId);
+}
