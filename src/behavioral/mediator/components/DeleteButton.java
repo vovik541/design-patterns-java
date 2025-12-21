cc086a5526e8471c9ae38a6657274c096cd080d9
vovik541 <conanzhill@gmail.com>
2025-12-20 20:35:47 -0800

    mediator example reviewed & added

---
+++ src/behavioral/mediator/components/DeleteButton.java
@@ -0,0 +1,29 @@
+package behavioral.mediator.components;
+
+import behavioral.mediator.mediator.Mediator;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class DeleteButton extends JButton  implements Component {
+    private Mediator mediator;
+
+    public DeleteButton() {
+        super("Del");
+    }
+
+    @Override
+    public void setMediator(Mediator mediator) {
+        this.mediator = mediator;
+    }
+
+    @Override
+    protected void fireActionPerformed(ActionEvent actionEvent) {
+        mediator.deleteNote();
+    }
+
+    @Override
+    public String getName() {
+        return "DelButton";
+    }
+}
---
+++ src/behavioral/mediator/components/SaveButton.java
@@ -0,0 +1,29 @@
+package behavioral.mediator.components;
+
+import behavioral.mediator.mediator.Mediator;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class SaveButton extends JButton implements Component {
+    private Mediator mediator;
+
+    public SaveButton() {
+        super("Save");
+    }
+
+    @Override
+    public void setMediator(Mediator mediator) {
+        this.mediator = mediator;
+    }
+
+    @Override
+    protected void fireActionPerformed(ActionEvent actionEvent) {
+        mediator.saveChanges();
+    }
+
+    @Override
+    public String getName() {
+        return "SaveButton";
+    }
+}
