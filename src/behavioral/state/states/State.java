91fd8614ce9cdce966f7f45f1090a2a33583689f
vovik541 <conanzhill@gmail.com>
2025-12-20 22:46:08 -0800

    state example reviewed & added

---
+++ src/behavioral/state/states/State.java
@@ -0,0 +1,16 @@
+package behavioral.state.states;
+
+import behavioral.state.ui.Player;
+
+public abstract class State {
+    Player player;
+
+    State(Player player) {
+        this.player = player;
+    }
+
+    public abstract String onLock();
+    public abstract String onPlay();
+    public abstract String onNext();
+    public abstract String onPrevious();
+}
